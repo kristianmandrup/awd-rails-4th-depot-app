26157a597b33069e3a36dad7650d28be01f9934f
Kristian Mandrup <kmandrup@gmail.com>
2010-06-15 09:11:11 +0200

    iteration 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  default_scope order('title')
+  
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do |map|
+  get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Depot::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
