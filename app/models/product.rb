0d8a248ea9b74e6df11c02a7eb40145606b45dc5
Kristian Mandrup <kmandrup@gmail.com>
2010-06-15 09:01:58 +0200

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with	=> %r{\.(gif|jpg|png)$}i, 
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -3,8 +3,14 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+      :title       => 'Lorem Ipsum',
+      :description => 'Wibbles are fun!',
+      :image_url   => 'lorem.jpg',
+      :price       => 19.95
+    }
   end
-
+  
   test "should get index" do
     get :index
     assert_response :success
@@ -18,7 +24,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, :product => @product.attributes
+      post :create, :product => @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +41,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, :id => @product.to_param, :product => @product.attributes
+    put :update, :id => @product.to_param, :product => @update
     assert_redirected_to product_path(assigns(:product))
   end
 
